38ec53c064f6464414b7a2368334a336404d8676
Mike Tourigny <mike@alchemybase.com>
2021-03-06 16:10:34 -0500

    mirror serum dex devnet

---
+++ src/utils/preferences.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { useLocalStorageState } from './utils';
+import { useInterval } from './useInterval';
+import { useConnection } from './connection';
+import { useWallet } from './wallet';
+import {
+  useAllMarkets,
+  useSelectedTokenAccounts,
+  useTokenAccounts,
+} from './markets';
+import { settleAllFunds } from './send';
+import { PreferencesContextValues } from './types';
+
+const PreferencesContext = React.createContext<PreferencesContextValues | null>(
+  null,
+);
+
+export function PreferencesProvider({ children }) {
+  const [autoSettleEnabled, setAutoSettleEnabled] = useLocalStorageState(
+    'autoSettleEnabled',
+    true,
+  );
+
+  const [tokenAccounts] = useTokenAccounts();
+  const { connected, wallet } = useWallet();
+  const [marketList] = useAllMarkets();
+  const connection = useConnection();
+  const [selectedTokenAccounts] = useSelectedTokenAccounts();
+
+  useInterval(() => {
+    const autoSettle = async () => {
+      const markets = (marketList || []).map((m) => m.market);
+      try {
+        if (!wallet) {
+          return;
+        }
+
+        console.log('Auto settling');
+        await settleAllFunds({
+          connection,
+          wallet,
+          tokenAccounts: tokenAccounts || [],
+          markets,
+          selectedTokenAccounts,
+        });
+      } catch (e) {
+        console.log('Error auto settling funds: ' + e.message);
+      }
+    };
+
+    connected && wallet?.autoApprove && autoSettleEnabled && autoSettle();
+  }, 10000);
+
+  return (
+    <PreferencesContext.Provider
+      value={{
+        autoSettleEnabled,
+        setAutoSettleEnabled,
+      }}
+    >
+      {children}
+    </PreferencesContext.Provider>
+  );
+}
+
+export function usePreferences() {
+  const context = useContext(PreferencesContext);
+  if (!context) {
+    throw new Error('Missing preferences context');
+  }
+  return {
+    autoSettleEnabled: context.autoSettleEnabled,
+    setAutoSettleEnabled: context.setAutoSettleEnabled,
+  };
+}
